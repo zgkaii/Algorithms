44701168ef09540b3e7cac243979083dd0f7ab35
Everglow <kaiz1204@163.com>
2021-01-19 21:43:45 +0800

    update 2020/01/19

---
+++ LeetCode/list/LRUCache.java
@@ -0,0 +1,24 @@
+package list;
+
+/**
+ * @Author: Mr.Z
+ * @DateTime: 2021/01/19 21:39
+ * @Description: 146. LRU 缓存机制 https://leetcode-cn.com/problems/lru-cache/
+ **/
+public class LRUCache {
+    private int capacity;
+    private int size;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void put(int key, int value) {
+        return;
+    }
+}
