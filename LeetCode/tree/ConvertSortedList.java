50e24061d5877b0ee18001044732943f23230583
zgkaii <2488784449@qq.com>
2021-03-02 08:33:50 +0800

    update

---
+++ LeetCode/tree/ConvertSortedList.java
@@ -0,0 +1,30 @@
+package tree;
+
+/**
+ * @Author: Mr.Z
+ * @DateTime: 2021/03/01 22:50
+ * @Description: 109. 有序链表转换二叉搜索树 https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/description/
+ **/
+public class ConvertSortedList {
+    public TreeNode1 sortedListToBST(ListNode1 head) {
+        return null;
+    }
+}
+
+class ListNode1 {
+    int val;
+    ListNode1 next;
+
+    ListNode1() {
+    }
+
+    ListNode1(int val) {
+        this.val = val;
+    }
+
+    ListNode1(int val, ListNode1 next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
